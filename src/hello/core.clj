753440d4a6400ee64e8e0a4fe9059a71580fff55
Athmika Hebbar <athmikah@gmail.com>
2021-07-21 23:31:06 +0530

    Add solution to 4clojure problem 39, 55
    Solutions to interleaving two sequences and counting word frequencies
    are added along with the tescases.

--- src/hello/core.clj
+++ src/hello/core.clj
@@ -1,5 +1,7 @@
 
+(require '[clojure.string :as str])
 (ns hello.core)
+
 
 (defn foo
   "I don't do a whole lot."
@@ -20,8 +22,8 @@
 (defn sumOfArray
   [seq]
   (if (empty? seq)
-    0)
-  (reduce + seq))
+    0
+    (reduce + seq)))
 
 (defn cycles
   [seq]
@@ -39,10 +41,10 @@
 (defn palindrome
   [seq]
   (if (empty? seq)
-    nil)
-  (if (= seq (apply str (reverse seq)))
-    true
-    false))
+    nil
+    (if (= seq (apply str (reverse seq)))
+      true
+      false)))
 
 (defn sum-of-sq
   [seq]
@@ -67,8 +69,12 @@
   [seq]
   (reduce conj () seq))
 
+
+;; make sure to include docstring for every function
+
+
 (defn fibo
-  "This function takes three values: `n`, `x` and `y`. This is a helper function for `fibo-seq`."
+  "This function takes three values: `n`, `x` and `y` and generates the `n`th fibonacci number . This is a helper function for `fibo-seq`."
   [n x y]
   (if (< n 2) x (fibo (- n 1) y (+ x y))))
 
@@ -84,3 +90,115 @@
 (defn count-elements
   [seq]
   (zipmap (distinct seq) (map #(count-occurence seq %) (distinct seq))))
+
+
+
+(def planets
+  [{:pname "Mercury" :moons 0  :mass 0.0533}
+   {:pname "Venus"   :moons 0  :mass 0.815}
+   {:pname "Earth"   :moons 1  :mass 1}
+   {:pname "Mars"    :moons 2  :mass 0.107}])
+
+
+
+(defn less-mass-than-earth?
+  [planet]
+  (< (:mass planet) 1))
+
+(defn less-than-one? 
+  [number x] 
+  (if (< x 1) 
+    (conj number x) 
+    number)
+  )
+
+(reduce less-than-one? [] [4 -5 -9 -1 3 ])
+(defn reduce-filter
+  [function init a]
+  (reduce 
+   (fn 
+     [number x] 
+     (if (function x)
+       (conj number x)
+       number))
+   init
+   a
+   ))
+
+(defn fizzbuzz
+  [n]
+  (cond (= 0 (mod n 15))
+        "FIZZBUZZ"
+        (= 0 (mod n 3))
+        "FIZZ"
+        (= 0 (mod n 5))
+        "BUZZ"
+        :else n))
+(defn generate-fizzbuzz
+  [n]
+  (vec (map fizzbuzz (range 1 (inc n)))))
+
+;; Problem 3: finding frequency of each word in a string
+(defn split
+  [string]
+  (str/split string #" "))
+
+(defn count-freq
+  [string word]
+  (count (filter #{word} (split string))))
+
+(defn count-words
+  [string]
+  (zipmap (distinct (split string)) (map #(count-freq string %) (distinct (split string)))))
+
+;;inteleave 2 sequences
+
+(defn interleave-seq
+  [seq1 seq2]
+  (mapcat vector seq1 seq2))
+
+;; [5 2 3 4] [1 1 1 1 1 1 1 1]
+;; [5 1 2 1 3 1 1 1 1 1 1]
+(defn interleave-seqs
+  [seq1 seq2]
+  (let [l (if (> (count seq1) (count seq2)) 1 2)]
+    (loop [init1 0
+           init2 0
+           res []]
+      (if (or (and (= l 1) (= (dec (count seq1))
+                              init1))
+              (and (= l 2) (= (dec (count seq2))
+                              init2)))
+        res
+        (recur (inc init1)
+               (inc init2) (cond (and (= l 1) (= (dec (count seq2)) init2))
+                                 (conj res (nth seq1 init1))
+                                 (and (= l 2) (= (dec (count seq1)) init1))
+                                 (conj res (nth seq2 init2))
+                                 :else (conj res (nth seq1 init1) (nth seq2 init2))))
+        ))))
+
+(defn interleave-seq2
+  [seq1 seq2]
+  (loop [res []
+         list1 seq1
+         list2 seq2]
+    (if (and (empty? list1) (empty? list2))
+      res
+      (let [elem1 (first list1)
+            elem2 (first list2)
+            r (cond (= nil elem1) (conj res elem2)
+                    (= nil elem2) (conj res elem1)
+                    :else (conj res elem1 elem2))]
+        (recur r (rest list1) (rest list2))))))
+
+
+
+(defn word-frequency
+  "Create a word frequency map out of a large string s.
+  `s` is a long string containing a lot of words"
+  [s]
+  (reduce
+    (fn [m w]
+      (assoc m w (inc (m w 0))))
+        {} (re-seq #"\w+" s)))
